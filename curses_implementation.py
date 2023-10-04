1eac379985ef7adba037cfc49b86f9c784292a15
Johannes C. Mayer <j.c.mayer240@gmail.com>
2023-10-04 19:22:52 +0200

    better formating

--- curses_implementation.py
+++ curses_implementation.py
@@ -5,25 +5,30 @@ import datetime
 import json
 import logging
 import math
+import os
 import re
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_output
 from pathlib import Path
 import textwrap
+from typing import Annotated
+import tempfile
 
 import pyperclip
 import typer
 
 app = typer.Typer()
-
 start_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 #transcription = Path("/home/johannes/Videos/2023-10-03 16-01-13_arun_accidental_alien_actress_transcriptions/2023-10-03 16-01-13_arun_accidental_alien_actress.vtt")
 #video = Path("/home/johannes/Videos/2023-10-03 16-01-13_arun_accidental_alien_actress.mkv")
 mpv_socket = Path(f"/tmp/mpvsocket")
 
-logging.basicConfig(level=logging.DEBUG)
+project_dir = Path(__file__).parent
+log_path = project_dir / "log"
+logging.basicConfig(level=logging.INFO, filename=f"{log_path}", filemode="w", format="%(asctime)s %(levelname)s %(message)s")
 
-logging.debug(f"Using mpv socket: {mpv_socket}")
+### MPV IPC ###
+logging.info(f"Using mpv socket: {mpv_socket}")
 
 def set_playback_position(pos):
     Popen(f'echo \'{{ "command": ["set_property", "playback-time", "{pos}"], "async": true }}\' | socat - {mpv_socket}', shell=True, stdout=PIPE, stderr=PIPE)
@@ -51,6 +56,21 @@ def set_pause(pause: bool):
         play_v = "false"
     Popen(f'echo \'{{ "command": ["set_property", "pause", {play_v}], "async": true }}\' | socat - {mpv_socket}', shell=True, stdout=PIPE, stderr=PIPE)
 
+### Transcription ###
+def load_transcription(transcription_path: Path):
+    if transcription_path.suffix == ".vtt":
+        logging.info("Opening transcription file")
+        with transcription_path.open() as f:
+            return f.readlines()
+    elif transcription_path.suffix == ".mkv":
+        logging.info("Extracting subtitles from video file")
+        tmp_file = tempfile.NamedTemporaryFile(suffix=".vtt")
+        check_output(f'ffmpeg -y -i "{transcription_path}" -map 0:s:0 {tmp_file.name}', shell=True)
+        with open(tmp_file.name) as f:
+            return f.readlines()
+    else:
+        raise Exception(f"Unknown file type: {transcription_path.suffix}")
+
 def display_transcription(stdscr, lines, offset):
     stdscr.clear()
     escape = False
@@ -60,10 +80,14 @@ def display_transcription(stdscr, lines, offset):
             if y >= curses.LINES-1 and (c == '\n' or x >= curses.COLS-1):
                 escape = True
                 break
-            stdscr.addstr(c)
+            if is_timestamp_line(line):
+                stdscr.addstr(c, curses.color_pair(240))
+            else:
+                stdscr.addstr(c)
         if escape:
             break
 
+### Curses ###
 def clamp_offset(offset, lines):
     return min(len(lines), max(0, offset))
 
@@ -74,25 +98,36 @@ def wrap_lines(lines):
             new_lines.append(i + "\n")
     return new_lines
 
-def main(stdscr, transcription: Path):
+def is_timestamp_line(line):
+    return re.match(r"(\d{2}:)?\d\d:\d\d\.\d\d\d --> (\d{2}:)?\d\d:\d\d\.\d\d\d", line)
+
+def additional_curses_setup():
+    curses.start_color()
+    curses.use_default_colors()
+    logging.info(f"Colors supported: {curses.COLORS}")
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
+
     curses.mousemask(-1)
     curses.mouseinterval(0)
 
-    offset = 0
+def main(stdscr, transcription: Path):
+    additional_curses_setup() 
 
-    with transcription.open() as f:
-        lines = f.readlines()
-
-    lines = list(wrap_lines(lines))
-
-    display_transcription(stdscr, lines, offset)
+    vanilla_lines = load_transcription(transcription)
+    lines = list(wrap_lines(vanilla_lines))
 
     #Popen(["mpv", f"--input-ipc-server='{mpv_socket}'", "--speed=1.8", f"--sub-files='{transcription}'", f"{video}"], stdout=PIPE, stderr=PIPE)
+
+    offset = 0
+    display_transcription(stdscr, lines, offset)
 
     mouse_down_coords = (0, 0)
     while True:
         event = stdscr.getch()
-        curses.update_lines_cols()
+
+        logging.debug(f"Event: {event}")
+
         if event == ord("q"): 
             break 
         if event == ord("j"):
@@ -109,13 +144,20 @@ def main(stdscr, transcription: Path):
             display_transcription(stdscr, lines, offset)
         if event == ord("p"):
             set_pause("toggle")
+        if event == curses.KEY_RESIZE:
+            logging.debug("Resizing")
+            curses.update_lines_cols()
+            lines = list(wrap_lines(vanilla_lines))
+            display_transcription(stdscr, lines, offset)
         if event == curses.KEY_MOUSE:
             _, mx, my, _, bstate = curses.getmouse()
             if (bstate & curses.BUTTON3_PRESSED != 0 or
                 bstate & curses.BUTTON3_CLICKED != 0):
                 clicked_line = lines[offset+my]
-                while not re.match(r"(\d{2}:)?\d\d:\d\d\.\d\d\d", clicked_line):
+                while not is_timestamp_line(clicked_line):
                     my -= 1
+                    if offset+my < 0:
+                        break
                     clicked_line = lines[offset+my]
                 # parse based on if hours are present in the timestamp
                 if clicked_line[10:13] == "-->":
@@ -129,20 +171,28 @@ def main(stdscr, transcription: Path):
                 mouse_down_coords = (mx, my)
             # Copy selection to clipboard
             if bstate & curses.BUTTON1_RELEASED != 0:
+                start_idx = min(offset+mouse_down_coords[1], offset+my)
+                end_idx = max(offset+mouse_down_coords[1], offset+my)
                 result = ""
-                for s in lines[offset+mouse_down_coords[1]:offset+my+1]:
+                for s in lines[start_idx:end_idx+1]:
                     s = s.strip()
-                    if s == "" or re.match(r"(\d{2}:)?\d\d:\d\d\.\d\d\d", s):
+                    if s == "" or is_timestamp_line(s):
                         continue
                     result += s + " "
 
                 pyperclip.copy(result)
+                logging.info(f"Copied to clipboard: {result}")
 
     curses.endwin()
 
 
-def wrapper(file: Path):
+@app.command()
+def display(file: Annotated[Path, typer.Argument()]):
     curses.wrapper(lambda stdscr: main(stdscr, file))
 
+@app.command()
+def log():
+    os.system(f"tail -F {log_path}")
+
 if __name__ == "__main__":
-    typer.run(wrapper)
+    app()
